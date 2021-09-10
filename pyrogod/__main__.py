9e7515ac1d48f5bdc95c1977521eb6c492d6d0a9
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-09-10 19:32:34 +0530

    Create __main__.py

---
+++ pyrogod/__main__.py
@@ -0,0 +1,24 @@
+# (C) 2021 Paramendra Singh
+
+from pyrogram import idle, Client, filters
+from pyrogram.types import Message
+from pyrogod import app, LOGGER
+from config import *
+from pyrogod.modules import *
+import os
+
+
+async def pyro_on_fire(_, message: Message):
+  await app.send_message(LOG_CHAT, "**#SUCCESS**\n\n**Your PyroGod Deployed Successfully**\n\n**NOW ENJOY!!!**")
+
+
+
+
+
+print("PyroGod Started \nPowered By @Perry_XD\n(C) 2021 Paramedra Singh")
+
+
+
+
+if __name__ == "__main__":
+  app.run()
