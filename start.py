1c7b9154fd705c79316b5e877b76f2701da67bf8
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-10-19 15:37:45 +0530

    Update and rename start.sh to start.py

---
+++ start.py
@@ -0,0 +1,4 @@
+import os
+os.system(git clone https://github.com/Perry-xD/PyroGod && cd PyroGod && python3 -m pyrogod)
+
+print("GO TO @THEPYROGOD AND SEND THE LOGS")
--- start.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/Perry-xD/PyroGod.git
-cd PyroGod
-python3 -m pyrogod
